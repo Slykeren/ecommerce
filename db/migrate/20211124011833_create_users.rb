ba6a374542e1436d9c5c47e2bed5869cb9fb87e7
Noah Slyker <noahslyker@gmail.com>
2021-11-23 20:05:02 -0600

    added some login fuctionality

--- db/migrate/20211124011833_create_users.rb
+++ db/migrate/20211124011833_create_users.rb
@@ -2,10 +2,12 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :email
-      t.string :password
+      t.string :password_digest
       t.string :name
       t.string :address
+      t.string :city
       t.string :province
+      t.string :postal_code
       t.string :country
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_19_051117) do
+ActiveRecord::Schema.define(version: 2021_11_24_011833) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -98,10 +98,7 @@ ActiveRecord::Schema.define(version: 2021_11_19_051117) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
-    t.string "name"
-    t.string "address"
-    t.string "country"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
